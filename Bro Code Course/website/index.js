499eaa5c703741c6c9f61f1f4f808670131acb2f
Hugo Almeida <116381112+hugoalmeida02@users.noreply.github.com>
2025-01-29 22:24:41 +0000

    Update Bro Code Course

--- Bro Code Course/website/index.js
+++ Bro Code Course/website/index.js
@@ -1,1 +1,12 @@
-//44:56
+//52:16
+
+const PI = 3.14159
+let radius
+let circum
+
+document.getElementById("mySubmit").onclick = function() {
+    radius = document.getElementById("myText").value
+    radius = Number(radius)
+    circum = 2 * PI * radius
+    document.getElementById("myP").textContent = `Result: ${circum}`
+}
