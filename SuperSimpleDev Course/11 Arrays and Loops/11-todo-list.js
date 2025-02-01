efebae13a431ab82f4c2aac016e9087b3af0c10f
Hugo Almeida <116381112+hugoalmeida02@users.noreply.github.com>
2025-02-01 23:46:35 +0000

    Update 11-todo-list.js

--- SuperSimpleDev Course/11 Arrays and Loops/11-todo-list.js
+++ SuperSimpleDev Course/11 Arrays and Loops/11-todo-list.js
@@ -1,25 +1,5 @@
-let todoList = {
-    names: [],
-    dates: [],
-    index: 0
-}
+const todoList = []
 
-const addbtn = document.querySelector("#addbtn")
-
-addbtn.onclick = add
-
-function add() {
-    const todoName = document.querySelector("#name").value
-    const date = document.querySelector("#date").value
-
-    todoList.names.push(todoName)
-    todoList.names.push(date)
-    todoList.index++
-
-    const p = document.querySelector("#todoList").innerText = `<p id="p${todoList.index}">${todoName} ${date} <button id="deletebtn${todoList.index}" onclick = "del(${todoList.index})">Delete</button></p>`
-}
-
-function del(index) {
-    const p = document.querySelector("#todoList").innerHTML = null
-
+function addTodo() {
+    const inputElemne
 }
