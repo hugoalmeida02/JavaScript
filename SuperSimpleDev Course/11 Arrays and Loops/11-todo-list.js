3b39e093d8083e13848ddeab42087d751b497662
Hugo Almeida <116381112+hugoalmeida02@users.noreply.github.com>
2025-02-02 00:34:55 +0000

    Create TodoList

--- SuperSimpleDev Course/11 Arrays and Loops/11-todo-list.js
+++ SuperSimpleDev Course/11 Arrays and Loops/11-todo-list.js
@@ -1,5 +1,45 @@
-const todoList = []
+const todoList = [{
+    name : 'wash dishes',
+    date: "2025-02-02"
+}, {
+    name : 'make dinner',
+    date: "2025-02-02"
+}]
+
+renderTodoList()
+
+function renderTodoList() {
+    let todoListHTML = ''
+
+    for (let i = 0; i < todoList.length; i++) {
+        const HTML = `
+        <div>${todoList[i].name}</div>
+        <div>${todoList[i].date}</div>
+        <button class="delbtn" onclick="
+        todoList.splice(${i},1)
+        renderTodoList()
+        ">Delete</button>`
+        todoListHTML += HTML
+    }
+
+    console.log(todoListHTML)
+    document.querySelector('div#container').innerHTML = todoListHTML
+}
+
 
 function addTodo() {
-    const inputElemne
+    const inputElement = document.querySelector("#name")
+    const dateElement = document.querySelector("#date")
+    const name = inputElement.value
+    const date =  dateElement.value
+
+    todoList.push({
+        name,
+        date
+    })
+
+    inputElement.value = ''
+    dateElement.value = ''
+
+    renderTodoList()
 }
